9791c019b02de434319d584c17b446541369aca6
Viktor Szabo <viklukacs64@gmail.com>
2017-09-11 17:27:50 +0200

    Finished Exercises 22/35- Even

---
+++ week-02/day-1/print-even/src/PrintEven.java
@@ -0,0 +1,15 @@
+public class PrintEven {
+
+    public static void main(String[] args) {
+
+        //int i = 0;
+        //int even = 0;
+
+        for (int i = 1; i < 500; i+=2){
+
+                System.out.println(i);
+
+            }
+       }
+
+}
