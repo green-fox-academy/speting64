de7ce877528b09d87c6d156ba3ac278017760c7e
Viktor Szabo <viklukacs64@gmail.com>
2017-09-11 17:50:22 +0200

    Finished Exercises 22/35- DrawTriangle added

---
+++ week-02/day-1/draw-triangle/src/DrawTriangle.java
@@ -0,0 +1,15 @@
+// Write a program that reads a number from the standard input, then draws a
+// triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as the number was
+
+
+public class DrawTriangle {
+
+
+}
