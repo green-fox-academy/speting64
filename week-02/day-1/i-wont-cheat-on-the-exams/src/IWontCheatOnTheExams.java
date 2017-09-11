594e45922a35f241272d2f6ebc304cc9a5b4b681
Viktor Szabo <viklukacs64@gmail.com>
2017-09-11 17:00:48 +0200

    Finished Exercises 21/35- Conditional

---
+++ week-02/day-1/i-wont-cheat-on-the-exams/src/IWontCheatOnTheExams.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class IWontCheatOnTheExams {
+
+    public static void main(String[] args){
+
+        int i=0;
+        while(i <100)
+        {
+            System.out.println("I won't cheat on the exams"); // Prints the numbers from 0 to 9
+            i++;
+        }
+
+    }
+
+}
