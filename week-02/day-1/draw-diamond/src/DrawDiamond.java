d69814b02f24b34c2f3eec6201021007d94abd6d
Viktor Szabo <viklukacs64@gmail.com>
2017-09-11 17:58:43 +0200

    Finished Exercises 25/35- DrawDiamond added

---
+++ week-02/day-1/draw-diamond/src/DrawDiamond.java
@@ -0,0 +1,16 @@
+// Write a program that reads a number from the standard input, then draws a
+// diamond like this:
+//
+//
+//    *
+//   ***
+//  *****
+// *******
+//  *****
+//   ***
+//    *
+//
+// The diamond should have as many lines as the number was
+
+public class DrawDiamond {
+}
