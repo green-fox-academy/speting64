0c030baa658bb20f00c21ff5833a1424e213fd61
Viktor Szabo <viklukacs64@gmail.com>
2017-09-11 16:07:52 +0200

    Finished exercises till two-numbers

---
+++ home/speting64/greenfox/speting64/week-02/day-1/exercise-01/src/HelloMe.java
@@ -0,0 +1,10 @@
+
+
+public class HelloMe
+{
+    public static void main(String[] args)
+    {
+        // Modify this program to greet you instead of the World!
+        System.out.println("Hello Viktor");
+    }
+}
---
+++ home/speting64/greenfox/speting64/week-02/day-1/two-numbers/src/TwoNumbers.java
@@ -0,0 +1,34 @@
+public class TwoNumbers {
+    public static void main(String[] args) {
+        // Create a program that prints a few operations on two numbers: 22 and 13
+        int a = 22;
+        int b = 13;
+        // Print the result of 13 added to 22
+
+        int sum = a+b;
+        System.out.println(sum);
+
+        // Print the result of 13 substracted from 22
+
+        int sub = a-b;
+        System.out.println(sub);
+
+        // Print the result of 22 mutiplied by 13
+
+        int mux = a*b;
+        System.out.println(mux);
+
+        // Print the result of 22 divided by 13 (as a decimal fraction)
+
+        int div = a/b;
+        System.out.println(div);
+
+        // Print the integer part of 22 divided by 13
+
+        // Print the reminder of 22 divided by 13
+
+        int rem = a%b;
+        System.out.println(rem);
+
+    }
+}
