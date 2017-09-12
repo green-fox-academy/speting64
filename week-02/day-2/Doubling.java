331c91d9d9d58d9492c5c41c90b6946d227f3cf9
Viktor Szabo <viklukacs64@gmail.com>
2017-09-12 12:57:57 +0200

    Finished Exercises 1/19 Doubling added

---
+++ week-02/day-2/Doubling.java
@@ -0,0 +1,18 @@
+public class Doubling {
+
+    // Creating array with ten elements
+    public static void main(String[] args) {
+
+        int ak =123;
+        doublingFunction(ak);
+
+    }
+    public static void doublingFunction(int ak){
+
+        int doubling;
+        doubling = ak * 2;
+        System.out.println("Doubling = " + doubling);
+    }
+
+
+}
