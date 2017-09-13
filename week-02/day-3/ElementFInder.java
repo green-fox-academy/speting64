85bfeed14532bc79d6ba427f3b933411dee1a248
Viktor Szabo <viklukacs64@gmail.com>
2017-09-13 14:30:22 +0200

    Finished Exercises 9/12+3 ElementFInder added

---
+++ week-02/day-3/ElementFInder.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+// Check if the arrayList contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+// Do this again with a different solution using different list functions!
+
+public class ElementFInder {
+
+    public static void main(String... args) {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
+
+        // Solution 1
+
+        if(arrayList.contains(7)){
+
+            System.out.println("Hoorray");
+        }
+        else{
+
+            System.out.println("Nooooooooo");
+        }
+        // Solution 2
+
+    }
+}
