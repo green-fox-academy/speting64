df4a461201d1e801c76e8027cb01b0f99e323283
Viktor Szabo <viklukacs64@gmail.com>
2017-09-13 17:03:20 +0200

    Finished Exercises 12/12+3 Reverse done

--- week-02/day-3/Reverse.java
+++ week-02/day-3/Reverse.java
@@ -2,23 +2,24 @@
 // Use it on this reversed string to check it!
 // Try to solve this using charAt() first, and optionally anything else after.
 
-/*
+
 public class Reverse {
 
     public static void main(String[] args){
 
         String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
-        System.out.println(reserver(reversed));
+        System.out.println(reverser(reversed));
 
     }
+
     public static String reverser(String source) {
 
 
-        for (int i = (len - 1); i < source.length; i++){
-            dest.append(source.charAt(i));
+        String reversedSource = " ";
+        for (int i = source.length() - 1; i >= 0; i--){
+            reversedSource += source.charAt(i);
         }
-
-        return dest.toString();
+        return reversedSource;
     }
 }
-*/
+
