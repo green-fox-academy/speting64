a106ca92622acd302af4322b298669b269e73283
Viktor Szabo <viklukacs64@gmail.com>
2017-09-13 10:42:51 +0200

    Finished Exercises 1/12+3 SimpleReplace added

---
+++ week-02/day-3/SimpleReplace.java
@@ -0,0 +1,11 @@
+
+
+public class SimpleReplace {
+
+    public static void main(String[] args){
+
+        String example = "In a dishwasher far far away";
+        String replaceString = example.replace("dishwasher","galaxy");
+        System.out.println(replaceString);
+    }
+}
