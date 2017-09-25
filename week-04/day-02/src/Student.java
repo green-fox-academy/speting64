699a4446cbaaaac355f6707e38acc913b5decc84
Viktor Szabo <viklukacs64@gmail.com>
2017-09-25 20:29:13 +0200

    Student

---
+++ week-04/day-02/src/Student.java
@@ -0,0 +1,41 @@
+public class Student {
+
+    String name;
+    int age;
+    String gender;
+    String previousOrganization;
+    int skippedDays;
+
+    public Student(String previousOrganization, int skippedDays){
+        this.previousOrganization = previousOrganization;
+        this.skippedDays= skippedDays;
+    }
+    public Student() {
+        this.previousOrganization = "The School of Life ";
+        this.skippedDays = 0;
+    }
+    public void getGoal(){
+        System.out.println("Be a junior software developer.");
+    }
+    public void introduce(){
+        Student jane = new Student();
+        jane.name = "Jane Doe";
+        jane.age = 30;
+        jane.gender = "Female";
+        System.out.println("Hi, I'm "+ jane.name + " a " +jane.age + " years old " +jane.gender + " from " + jane.previousOrganization + "nwho skipped "
+                + jane.skippedDays +  " days from the course already.");
+    }
+
+    public void skipDays(int numberOfDays){
+        skippedDays += numberOfDays;
+    }
+}
+class Jane {
+    public static void main(String[] args){
+        Student jane = new Student();
+        jane.introduce();
+        jane.getGoal();
+        jane.skipDays(2);
+        System.out.println("Skipped days:"+ jane.skippedDays);
+    }
+}
