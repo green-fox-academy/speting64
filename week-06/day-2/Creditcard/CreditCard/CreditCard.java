9c30e17206e98cd0982605049b9cb67e99b1faa1
Viktor Szabo <viklukacs64@gmail.com>
2017-10-17 17:37:25 +0200

    Creditcard -- CheckSum works

---
+++ week-06/day-2/Creditcard/CreditCard/CreditCard.java
@@ -0,0 +1,50 @@
+package CreditCard;
+
+import java.util.Random;
+
+public class CreditCard implements CreditCardy {
+
+    int counter;
+
+    public int getSumCVV(){
+        return 0;
+    }
+
+    @Override
+    public String getNameCardholder() {
+        String cardUser = "ABC";
+        cardUser = cardUser.concat(String.valueOf(counter));
+        counter++;
+        return cardUser;
+    }
+
+    @Override
+    public String getCodeAccount() {
+        return getRandomCreditNumber();
+    }
+
+    @Override
+    public int cumeSumCVV(String codeAccount) {
+        int sumCVV =0;
+        for (int i = 0; i <codeAccount.length() ; i++) {
+            sumCVV += Character.getNumericValue(codeAccount.charAt(i));
+        }
+        return sumCVV;
+    }
+
+    String getRandomCreditNumber(){
+        String randomCardNumber = "";
+        for (int i = 0; i < 16 ; i++) {
+            randomCardNumber += String.valueOf(new Random().nextInt()*10);
+
+        }
+        return randomCardNumber;
+    }
+
+    public String toString(){
+        String codeAcc = getCodeAccount();
+        return String.format("Name=%s CC#=%s CVV=%d (validated)", getNameCardholder(), codeAcc,
+                        cumeSumCVV(codeAcc));
+    }
+
+}
---
+++ week-06/day-2/Creditcard/CreditCardApp.java
@@ -0,0 +1,14 @@
+import CreditCard.*;
+
+public class CreditCardApp {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10 ; i++) {
+
+            CreditCard credit = new CreditCard();
+            System.out.println(credit.toString());
+
+        }
+    }
+
+}
