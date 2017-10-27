a0a7c39e7330de7b1031e9ee16c0a9857ebf62e3
Viktor Szabo <viklukacs64@gmail.com>
2017-10-27 10:24:51 +0200

    FoxClub -- >  Route created in Controller

---
+++ week-07/day-4/programmerfoxclub/src/main/java/com/example/greenfox/programmerfoxclub/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.example.greenfox.programmerfoxclub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value="/")
+    public String index(Model model){
+        return "index";
+    }
+}
