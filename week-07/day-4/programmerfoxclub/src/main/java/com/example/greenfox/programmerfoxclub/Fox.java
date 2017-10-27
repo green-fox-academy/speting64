ff462f3e706d129d50fe38683c0f2ef4cfb42f9f
Viktor Szabo <viklukacs64@gmail.com>
2017-10-27 11:01:14 +0200

    FoxClub -- >  Fox added to the scene , if doesn't know tricks, the site will print it out

---
+++ week-07/day-4/programmerfoxclub/src/main/java/com/example/greenfox/programmerfoxclub/Fox.java
@@ -0,0 +1,55 @@
+package com.example.greenfox.programmerfoxclub;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@Component
+public class Fox {
+
+
+
+    String name;
+    String food;
+    String drink;
+    List<String> tricks;
+
+    public Fox(){
+        this.name = "Vuk";
+        this.food = "Goose";
+        this.drink = "Mud";
+        this.tricks = new ArrayList<>();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public String getDrink() {
+        return drink;
+    }
+
+    public List<String> getTricks() {
+        return tricks;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public void setDrink(String drink) {
+        this.drink = drink;
+    }
+
+
+}
--- week-07/day-4/programmerfoxclub/src/main/java/com/example/greenfox/programmerfoxclub/controller/MainController.java
+++ week-07/day-4/programmerfoxclub/src/main/java/com/example/greenfox/programmerfoxclub/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.example.greenfox.programmerfoxclub.controller;
 
+import com.example.greenfox.programmerfoxclub.Fox;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
+    @Autowired
+    Fox fox;
+
+
     @RequestMapping(value="/")
     public String index(Model model){
+        model.addAttribute("fox",fox);
         return "index";
     }
 }
