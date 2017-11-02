630b009d1c45404563981aa590ba08958443efdd
Viktor Szabo <viklukacs64@gmail.com>
2017-11-02 16:26:01 +0100

    Assignee --> Adding new values to the table implemented

--- week-08/day-2/connectionwithmysql/src/main/java/com/greenfox/connectionwithmysql/controller/AssigneeController.java
+++ week-08/day-2/connectionwithmysql/src/main/java/com/greenfox/connectionwithmysql/controller/AssigneeController.java
@@ -1,5 +1,6 @@
 package com.greenfox.connectionwithmysql.controller;
 
+import com.greenfox.connectionwithmysql.model.Assignee;
 import com.greenfox.connectionwithmysql.repository.AssigneeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,4 +19,17 @@ public class AssigneeController {
         model.addAttribute("assignee", assigneeRepository.findAll());
         return "assignees";
     }
+
+    @GetMapping("/addassignee")
+    public String add(Model model){
+        model.addAttribute("newbie" , new Assignee());
+        return "add";
+    }
+
+    @PostMapping("/addassignee")
+    public String addAssignee(@ModelAttribute Assignee assignee){
+        assigneeRepository.save(assignee);
+        return "redirect:/assignees/";
+    }
 }
+
