58c62a7022e0f08ac61ee53b88117c527255d61c
Viktor Szabo <viklukacs64@gmail.com>
2017-11-03 13:15:22 +0100

    RedditClone --> Downvote / Upvote Possible

--- week-08/day-4/reddit/src/main/java/com/greenfox/reddit/controller/MainController.java
+++ week-08/day-4/reddit/src/main/java/com/greenfox/reddit/controller/MainController.java
@@ -1,17 +1,16 @@
 package com.greenfox.reddit.controller;
 
 import com.greenfox.reddit.model.Post;
+import com.greenfox.reddit.service.Posting;
 import com.greenfox.reddit.repository.RedditRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("/posts")
@@ -21,6 +20,9 @@ public class MainController {
 
     @Autowired
     RedditRepository redditRepository;
+
+    @Autowired
+    Posting posting;
 
     @GetMapping(value = {"", "/"})
     public String listPost(Model model) {
@@ -38,6 +40,18 @@ public class MainController {
     public String addPost(@ModelAttribute Post post){
         redditRepository.save(post);
         return "redirect:/posts/";
+    }
+
+    @GetMapping("/{id}/upvote")
+    public String voteUp(@PathVariable (value = "id") long id) {
+        posting.upVote(id);
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/{id}/downvote")
+    public String voteDown(@PathVariable (value = "id") long id) {
+        posting.downVote(id);
+        return "redirect:/posts";
     }
 
 }
---
+++ week-08/day-4/reddit/src/main/java/com/greenfox/reddit/service/Posting.java
@@ -0,0 +1,34 @@
+package com.greenfox.reddit.service;
+
+import com.greenfox.reddit.model.Post;
+import com.greenfox.reddit.repository.RedditRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class Posting {
+
+    @Autowired
+    RedditRepository redditRepository;
+
+    public void upVote(long id){
+        Post post = redditRepository.findOne(id);
+        post.setScore(post.getScore()+1);
+        redditRepository.save(post);
+    }
+
+    public void downVote(long id){
+        Post post = redditRepository.findOne(id);
+        post.setScore(post.getScore()-1);
+        redditRepository.save(post);
+    }
+
+
+
+
+
+}
