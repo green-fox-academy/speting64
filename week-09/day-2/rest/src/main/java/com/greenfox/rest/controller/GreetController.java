b988b8a969e10a6f5d3930aa08d576fdaa09cfff
Viktor Szabo <viklukacs64@gmail.com>
2017-11-07 13:45:42 +0100

    Greeter --> Error handling works...kinda

--- week-09/day-2/rest/src/main/java/com/greenfox/rest/controller/GreetController.java
+++ week-09/day-2/rest/src/main/java/com/greenfox/rest/controller/GreetController.java
@@ -3,6 +3,8 @@ package com.greenfox.rest.controller;
 import com.greenfox.rest.model.Greeter;
 import com.greenfox.rest.model.Error;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +23,13 @@ public class GreetController {
         greeter.setName(name);
         greeter.setTitle(title);
         return greeter;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public Error errorMessage(MissingServletRequestParameterException e){
+        String parameterName=e.getParameterName();
+        error.setError("Please provide a "+ parameterName + "!");
+        return error;
     }
 
 
---
+++ week-09/day-2/rest/src/main/java/com/greenfox/rest/model/AppendA.java
@@ -0,0 +1,4 @@
+package com.greenfox.rest.model;
+
+public class AppendA {
+}
