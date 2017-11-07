f05273fc7bcc81680796094d78a1b66637b3d13f
Viktor Szabo <viklukacs64@gmail.com>
2017-11-07 12:01:10 +0100

    Doubling -->error handling working on strings

--- week-09/day-2/rest/src/main/java/com/greenfox/rest/controller/RESTController.java
+++ week-09/day-2/rest/src/main/java/com/greenfox/rest/controller/RESTController.java
@@ -1,6 +1,8 @@
 package com.greenfox.rest.controller;
 
 import com.greenfox.rest.model.Doubling;
+import com.greenfox.rest.model.Error;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,11 +12,25 @@ public class RESTController {
     @Autowired
     Doubling doubling;
 
+    @Autowired
+    Error error;
+
     @GetMapping("/doubling{input}")
     public Doubling Double(@RequestParam(value="input") int input){
+
+        if(input <= 0){
+            error.setError("Please provide a new input");
+        }
         doubling.setReceived(input);
+
         return doubling;
     }
 
-    
+    @ExceptionHandler(Exception.class)
+    public Error errorMessage(Exception e){
+        error.setError("Please provide a new input");
+        return error;
+    }
+
+
 }
---
+++ week-09/day-2/rest/src/main/java/com/greenfox/rest/model/Error.java
@@ -0,0 +1,27 @@
+package com.greenfox.rest.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Error {
+
+    String error;
+
+    public Error(){
+
+    }
+
+    public Error(String error) {
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+
+}
